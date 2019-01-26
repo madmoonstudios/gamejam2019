3443c71a1f0799f69942be6a9bdf7ffe5303c556
phelow <wpheloung@gmail.com>
2019-01-26 10:25:14 -0800

    Multiple interactions

--- Assets/Scripts/PlayerInteractionManager.cs
+++ Assets/Scripts/PlayerInteractionManager.cs
@@ -13,9 +13,17 @@ public class PlayerInteractionManager : MonoBehaviour
     [SerializeField]
     private ProgressBarPro _pentagramRechargeProgress;
     
+    [SerializeField]
+    private ProgressBarPro _vaseRechargeProgress;
+
     private const float c_pentagramMaxTime = 10.0f;
-    
+    private const float c_vaseMaxTime = 10.0f;
+
     private float _pentagramRemainingTime = 0.0f;
+
+    private bool _clickProcessed = false;
+
+    private float _vaseRemainingTime = 0.0f;
 
     private float _currentFear;
 
@@ -29,10 +37,41 @@ public class PlayerInteractionManager : MonoBehaviour
     {
         _pentagramRechargeProgress.SetValue(_pentagramRemainingTime / c_pentagramMaxTime);
         _pentagramRemainingTime -= Time.deltaTime;
+
+
+        _vaseRechargeProgress.SetValue(_vaseRemainingTime / c_vaseMaxTime);
+        _vaseRemainingTime -= Time.deltaTime;
+    }
+
+    private void LateUpdate()
+    {
+        _clickProcessed = false;
+    }
+
+    public bool TryBreakVase(Vase vase)
+    {
+        if (_clickProcessed)
+        {
+            return false;
+        }
+
+        if (_vaseRemainingTime > 0)
+        {
+            return false;
+        }
+
+        vase.Break();
+        _clickProcessed = true;
+        return true;
     }
 
     public bool TryDropPentagram()
     {
+        if (_clickProcessed)
+        {
+            return false;
+        }
+
         if (_pentagramRemainingTime > 0)
         {
             return false;
@@ -44,7 +83,7 @@ public class PlayerInteractionManager : MonoBehaviour
 
         Instantiate(_pentagram, adjustedWorldPos, Quaternion.identity);
         _pentagramRemainingTime = c_pentagramMaxTime;
-
+        _clickProcessed = true;
         return true;
     }
 }
