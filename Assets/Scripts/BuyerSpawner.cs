e194283d5333811865d02dbf1fc7ecd12d454e5c
AlvaroHG <alvaroh@MacBook-Pro-4.local>
2019-01-26 11:09:00 -0800

    Spawner

---
+++ Assets/Scripts/BuyerSpawner.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[InitializeOnLoad]
+public class BuyerSpawner : MonoBehaviour
+{
+    // Use this for initialization
+    static void Start()
+    {
+        print("Startup");
+        GameObject myRoadInstance =
+            Instantiate(Resources.Load("Buyer"),
+            new Vector3(5, 5, 5),
+            Quaternion.identity) as GameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
