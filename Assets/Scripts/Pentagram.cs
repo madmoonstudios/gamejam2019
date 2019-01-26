78aeaf621e8361b06ca87488a11bacec02961815
phelow <wpheloung@gmail.com>
2019-01-25 22:53:18 -0800

    Fearable logic

--- Assets/Scripts/Pentagram.cs
+++ Assets/Scripts/Pentagram.cs
@@ -11,10 +11,16 @@ public class Pentagram : MonoBehaviour
 
     [SerializeField]
     private int _maxTimeSeconds;
+    
+    private float _fearRadius;
+
+    [SerializeField]
+    private MeshRenderer _collisionMesh;
 
     // Start is called before the first frame update
     void Start()
     {
+        _fearRadius = _collisionMesh.transform.localScale.x;
         StartCoroutine(ScareThenDissapear());
     }
 
@@ -22,8 +28,23 @@ public class Pentagram : MonoBehaviour
     {
         for (int timeLeft = _maxTimeSeconds; timeLeft > 0; timeLeft--)
         {
+            _collisionMesh.enabled = false;
             _countdownText.text = timeLeft.ToString();
-            yield return new WaitForSeconds(1.0f);
+
+            yield return new WaitForSeconds(0.9f);
+            RaycastHit2D [] hits = Physics2D.CircleCastAll(this.transform.position, _fearRadius, Vector2.zero);
+
+            foreach (RaycastHit2D hit in hits)
+            {
+                IFearable fearable = hit.transform.GetComponent<IFearable>();
+                if (fearable != null)
+                {
+                    fearable.Scare();
+                }
+            }
+
+            _collisionMesh.enabled = true;
+            yield return new WaitForSeconds(0.1f);
         }
 
         Destroy(this.gameObject);
