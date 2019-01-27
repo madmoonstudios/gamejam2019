8d0ec2cf294519497f477e0b07c74462ad63bee1
phelow <wpheloung@gmail.com>
2019-01-26 18:12:46 -0800

    fixes

--- Assets/Scripts/Pentagram.cs
+++ Assets/Scripts/Pentagram.cs
@@ -9,15 +9,12 @@ public class Pentagram : FearInducer
     [SerializeField]
     private int _maxTimeSeconds;
     
-    private float _fearRadius;
-
     [SerializeField]
-    private MeshRenderer _collisionMesh;
+    private float _fearRadius;
 
     // Start is called before the first frame update
     void Start()
     {
-        _fearRadius = _collisionMesh.transform.localScale.x;
         StartCoroutine(ScareThenDissapear());
     }
 
@@ -27,13 +24,9 @@ public class Pentagram : FearInducer
 
         for (int timeLeft = _maxTimeSeconds; timeLeft > 0; timeLeft--)
         {
-            _collisionMesh.enabled = false;
-
             yield return new WaitForSeconds(0.9f);
             
             base.ScareInRadius(this.transform.position, _fearRadius);
-
-            _collisionMesh.enabled = true;
             yield return new WaitForSeconds(0.1f);
         }
 
