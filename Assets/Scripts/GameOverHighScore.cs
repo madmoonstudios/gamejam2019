3923c81195d1d403d4acd2e686ad1837e89d1843
Fernando Reyes <fereyesm@microsoft.com>
2019-01-27 13:49:07 -0800

    logo`

--- Assets/Scripts/GameOverHighScore.cs
+++ Assets/Scripts/GameOverHighScore.cs
@@ -11,7 +11,7 @@ public class GameOverHighScore : MonoBehaviour
     void Start()
     {
         _gameOverText.text = string.Format(
-            "House Haunters, High Score: {0}", 
+            "High Score: {0}", 
             PlayerPrefs.GetInt("HighScore", 0));
     }
 
