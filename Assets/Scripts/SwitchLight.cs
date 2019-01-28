a64ee9686b1dd6a8b1df2ba3a80f329336b519a6
dandov <dandovdel@gmail.com>
2019-01-27 17:01:53 -0800

    fixed lights

--- Assets/Scripts/SwitchLight.cs
+++ Assets/Scripts/SwitchLight.cs
@@ -35,6 +35,10 @@ public class SwitchLight : FearInducer, IClickable
 
     private IEnumerator FlickerLights()
     {
+        foreach (IFearable fearable in SwitchFearableContainer())
+        {
+            fearable.Scare(10.0f);
+        }
         isFlickering = true;
 
         int lightFlicks = UnityEngine.Random.Range(2, 5);
@@ -44,21 +48,15 @@ public class SwitchLight : FearInducer, IClickable
             _light.intensity = 0;
             yield return new WaitForSeconds(UnityEngine.Random.Range(.1f, .3f));
 
-
-            foreach (IFearable fearable in SwitchFearableContainer())
-            {
-                fearable.Scare(10.0f);
-            }
-
             _light.intensity = maxIntensity;
             yield return new WaitForSeconds(UnityEngine.Random.Range(.01f, .1f));
         }
 
         _light.intensity = 0;
         yield return new WaitForSeconds(3.0f);
-        base.ScareInRadius(this.transform.position, 20.0f);
-        _light.intensity = maxIntensity;
         isFlickering = false;
+        
+        _light.intensity = maxIntensity;
     }
 
     private void OnMouseEnter()
